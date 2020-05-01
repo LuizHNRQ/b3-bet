3bd2f520f80c6bf9fe2409456f53c57367927c3c
Luiz Henrique Barros Teixeira <luizhbtluiz@gmail.com>
2020-05-01 09:40:54 -0300

    feat: added six months rentability

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,22 @@
 const stockCode = document.querySelector("#stockCode");
 const btnSubmit = document.querySelector("#btn");
+const showSixMonths = document.querySelector("#sixMonths");
+
+//Global Variables
+const predefinedInvest = 1000;
+//let lastSixMonths; //Array of the Last six month of data
+
 
 //criar 5 fetchs com cada um usando uma Key diferente
 async function requestB3(stockCode) {
-  const stockLabels = [];
+  const stocksLabelDate = [];
   const stockPrice = [];
 
   const res = await fetch(
     `https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY_ADJUSTED&symbol=${stockCode}.SA&outputsize=compact&apikey=WTPIMEDHPNGBKRF5`
   );
   const data = await res.json();
+  console.log(data);
 
   //console.log('Resultado da requisição =', data);
 
@@ -17,27 +24,52 @@ async function requestB3(stockCode) {
   arrayKeys.reverse(); //Reverse the array to show in cronological order
 
   maxNumArray = arrayKeys.length; //get the number of months of the stocks in B3
-  const lastSixMonths = arrayKeys.slice(maxNumArray - 6); //selelct last 6 months
+  let lastSixMonths = arrayKeys.slice(maxNumArray - 6); //selelct last 6 months
 
   //Insert Date to the Chart
-  lastSixMonths.map(e => {
-    stockLabels.push(e);
+  lastSixMonths.map((e) => {
+    stocksLabelDate.push(e);
   });
 
   //Insert Price to the Chart
-  lastSixMonths.map(e => {
+  lastSixMonths.map((e) => {
     stockPrice.push(data["Monthly Adjusted Time Series"][`${e}`]["1. open"]);
   });
 
-  createChart(stockLabels, stockPrice); //Create new chart with the request data
+  createChart(stocksLabelDate, stockPrice); //Exib new Chart filled with the request data
+  calcSixMonths(lastSixMonths, stockPrice);
 }
+
+// -----> FUNCTIONS <-----
 
 // generate a random stock
 function randomStockNum(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-//Add Event Listner
+//calculate the Result of Six Months of Investment
+function calcSixMonths(lastSixMonths, stockPrice) {
+  let initialDate = lastSixMonths.slice(0, 1); //selelct ONLY the first month
+
+  let initialInvest = predefinedInvest;
+  let stocksBuyedInitialInvest = initialInvest / stockPrice[0];
+  let resultInvest = stocksBuyedInitialInvest * stockPrice[5];
+  let difInvest = resultInvest - initialInvest;
+
+  showSixMonths.innerHTML =
+    `6 Meses atrás(${initialDate}) em ${randomStock},
+     teria rendido = ${resultInvest}(${difInvest})`;
+
+  // DEBUG
+  /*
+  console.log(stocksBuyedInitialInvest);
+  console.log(stockPrice[5]);
+  console.log(stockPrice[0]);
+  */
+}
+
+
+// -----> EVENT LISTNER <-----
 btnSubmit.addEventListener("click", (e) => {
   e.preventDefault();
   console.log(stockCode.value);
